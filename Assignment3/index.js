782ad599e92d2470756f2fcd0b22cf4a3e685ab2
Emmanuel Perrin Joseph <ejoseph3@masonlive.gmu.edu>
2021-03-23 23:55:33 -0400

    Completed More Questions

--- Assignment3/index.js
+++ Assignment3/index.js
@@ -2,10 +2,14 @@
 'use strict';
 
 // Question 1
-function reverseNumber(number){
+const reverseNumber = function(number){
     if(!Number.isInteger(number) || !number )throw new Error("Error 🔴: Input should be a number");
     const strNumber = ''+number;
-    return +strNumber.split("").reverse().join("");
+    const outPut = +strNumber
+    .split("")
+    .reverse()
+    .join("");
+    return outPut;
 }
 console.log(reverseNumber(1122));
 
@@ -16,20 +20,25 @@ we will be using palindrome by String but the string can also contain other char
 */
 const palindrome = function(palindrome){
     if(!palindrome || typeof palindrome!='string')throw new Error("Error 🔴: Input should be a String");
-    const reverse = palindrome.split("").reverse().join("");
-    return reverse.toLowerCase()===palindrome.toLowerCase()? true: false;
+    const reverse = [...palindrome]
+    .reverse()
+    .join("");
+    const output = reverse.toLowerCase()===palindrome.toLowerCase()? true: false;
+    return output;
 }
 console.log(palindrome("PeEreep"))
 
 
 // Question 3
+// This line of code will remove spaces. But not mentioned in requirements. 
+// let ref = input.replace(/ /g,"");
 const combination = function(input){
     if(!input || typeof input!='string')throw new Error("Error 🔴: Input should be a String");
     let combinations =[];
-    // This line of code will remove spaces. But not mentioned in re// Quirements. 
-    // let ref = input.replace(/ /g,"");
     combinations.push(input);
-    input.split("").forEach((el,index,arr)=>{
+    [...input]
+    .forEach((el,index,arr)=>
+    {
         combinations.push(el);
         for(let x =index+1; x<arr.length;x++){
             combinations.push(`${el}${arr[x]}`);
@@ -43,20 +52,26 @@ console.log(combination("Dog"))
 // Question 4
 const reOrder = function(input){
     if(!input || typeof input!='string')throw new Error("Error 🔴: Input should be a String");
-    return input.split("").sort().join("");
+    const outPut = [...input]
+    .sort()
+    .join("");
+    return outPut;
 }
-console.log(reOrder("zdsrr"))
+console.log(reOrder("zzqas"))
 
 
 // Question 5
 const upperCaseCreater = function(input){
     if(!input || typeof input!='string')throw new Error("Error 🔴: Input should be a String");
-    return input.split(" ").map((el,index,arr)=>{
-        let strArr = el.split("");
+    const outPut = input
+    .split(" ")
+    .map((el,index,arr)=>{
+        let strArr = [...el];
         strArr[0]= strArr[0].toUpperCase();
         return strArr.join("")
     })
-    .join(" ");
+    .join(" ")
+    return outPut;
 }
 console.log(upperCaseCreater("aasdf hey comon"));
 
@@ -65,8 +80,11 @@ console.log(upperCaseCreater("aasdf hey comon"));
 const longestWord = function(input){
     if(!input || typeof input!='string')throw new Error("Error 🔴: Input should be a String");
     const lengthMap = new Map();
-    input.split(" ").forEach((el,index)=>lengthMap.set(el,el.length));
-    return [...lengthMap].sort((a,b)=>b[1]-a[1])[0][0]
+    input.split(" ")
+    .forEach((el,index)=>lengthMap.set(el,el.length));
+    const output= [...lengthMap]
+    .sort((a,b)=>b[1]-a[1])[0][0];
+    return output;
 }
 console.log(longestWord("Web Development Toutorial"));
 
@@ -74,7 +92,10 @@ console.log(longestWord("Web Development Toutorial"));
 // Question 7 
 const numberVowels = function(input){
     if(!input || typeof input!='string')throw new Error("Error 🔴: Input should be a String");
-    return[...input].filter(value=>'aeiou'.includes(value.toLowerCase())).length
+    const output = [...input]
+    .filter(value=>'aeiou'.includes(value.toLowerCase()))
+    .length
+    return output;
 }
 console.log(numberVowels("aeyytwhw"));
 
@@ -93,9 +114,95 @@ console.log(isPrime(13));
 
 // Question 9 
 const getType = (input)=>typeof input;
-console.log(getType(null));
+console.log(getType(()=>{}));
 
 
 // Question 10 
+//Identity matrix is a square matrix where all the elements in the principal diagonal are 1. Everything else 0;
+const getMatrix = function(n){
+    if(!Number.isInteger(n))throw new Error("Error 🔴: Input should be an Integer value")
+    const arrMatrix = [...Array(n)]
+    .map((val,index,arr)=>arr
+    .map((el,i,li)=>i===index?1:0))
+    return arrMatrix;
+}
+console.log(getMatrix(2));
 
+
+// Question 11
+//0's are also excepted here because it is a number.
+const secondHighestLowest = function(arr){
+    if(arr.length<=2 || !(arr instanceof Array) || !arr.every(el=>typeof el === 'number'))
+    throw new Error("Error 🔴: Input should be an Array and should have atleast 3 or more elements to get 2nd lowest");
+
+    if(arr.length===3)
+    return '' +arr[1]
+
+    if(arr.length>3){
+        const rearrange = [...arr].sort();
+        return rearrange[1]+"," +rearrange[rearrange.length-2]
+    }
+}
+console.log(secondHighestLowest([1,2,3,4,5]));
+
+
+//Question 12
+const perfectNumber = function(num){
+    if(num===1)return false;
+    if(!Number.isInteger(num) || num <=0 )
+    throw new Error("Error 🔴: Input should be a positive Integer");
+    let divisors = [];
+
+    //Getting divisors
+    for(let x=0; x<=num;x++){
+        if(num % x === 0)
+        divisors.push(x);
+    }
+
+    //first condition
+    const withoutNumberItSelf = divisors.slice(0,-1);
+    const firstCondition = num === withoutNumberItSelf.reduce((acc,cur)=>acc+cur);
+
+    //second condition
+    const secondCodition = num === divisors.reduce((acc,cur)=>acc+cur)/2
+
+    //final comparison
+    return firstCondition && secondCodition?true:false;
+    
+}
+console.log(perfectNumber(2));
+
+// Question 13
+const findFactors = function(num){
+    if(!Number.isInteger(num) || num <=0)
+    throw new Error("Error 🔴: Input should be a positive Integer greater than 0");
+    let factors = [];
+    for(let x = 1; x<=num;x++){
+        if(num % x ===0)
+        factors.push(x);
+    }
+   return factors;
+}
+console.log(findFactors(7));
+
+// Question 14
+const convertToCoins = function(amt,coins){
+    //purposly sorted the reference.
+    if(coins.sort((a,b)=>b-a)[0]>amt || !(typeof amt === 'number') || !coins.every(el=> typeof el ==='number'))
+    throw new Error("Error 🔴: Invalid Input");
+    let count = 0;
+    let arrCoins = [];
+    while(amt >0 && count<coins.length){
+        if(coins[count]<=amt){
+            amt-=coins[count];
+            arrCoins.push(coins[count]);
+        }
+        if(coins[count]>amt)
+            count++;
+    }
+    if(amt===0)
+    return arrCoins;
+    throw new Error("Error 🔴: The coins provided cannot add up to get the desired amt");
+}
+console.log(convertToCoins(55,[10,5]));
 
