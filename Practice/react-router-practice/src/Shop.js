73c78f67749dea2fefddd8d928babc72a0ac0977
Emmanuel Perrin Joseph <ejoseph3@masonlive.gmu.edu>
2021-05-03 11:09:14 -0400

    Changes made from hdd

--- Practice/react-router-practice/src/Shop.js
+++ Practice/react-router-practice/src/Shop.js
@@ -1,11 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
+import {Rating} from 'pat-ui'
+// import '/Users/emmanuel/.nvm/versions/node/v12.21.0/lib/node_modules/pat-ui/dist/index.css'
+import 'pat-ui/dist/index.css'
 
-export default class Shop extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Shop Page</h1>
-            </div>
-        )
-    }
+
+function Shop() {
+    const [rating,setRating] = useState(0);
+    return (
+        <div>
+            <h1>from Pat Ui</h1>
+            <Rating ratingtype="progress" barValue={30}/>
+            <Rating  defaultRating={4} barValue={30} getRating={(rating)=>setRating(rating)}/>
+            <br></br>
+            <span>rating: {rating}</span>
+            <Rating ratingtype="thumb"/>
+            <br></br>
+            <Rating ratingtype={'progress'} barcolor={{left:"pink",right:"orange"}} barValue={70}/>
+        </div>
+    )
 }
+
+export default Shop
+
